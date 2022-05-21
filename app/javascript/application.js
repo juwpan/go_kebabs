6d9892d823347cb3c740951392a4ede3089d7f16
juwpan <juwpan@gmail.com>
2022-05-21 22:14:51 +0400

    scripts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage/"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
 import * as bootstrap from "bootstrap"
