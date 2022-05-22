cf796fa4b2a838c491961ae276d21922d42b43ca
juwpan <juwpan@gmail.com>
2022-05-22 11:20:05 +0400

    Migrate comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   # Юзер может создавать много событий
   has_many :events, dependent: :delete_all
-  # has_many :comments, dependent: :delete_all
-  # has_many :subscriptions, dependent: :delete_all
+  has_many :comments, dependent: :delete_all
+  has_many :subscriptions, dependent: :delete_all
 
   gravtastic(secure: true, filetype: :png, size: 100, default: 'wavatar')
 
@@ -17,12 +17,12 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
-  # after_commit :link_subscriptions, on: :create
+  after_commit :link_subscriptions, on: :create
 
   private
 
-  # def link_subscriptions
-  #   Subscription.where(user_id: nil, user_email: self.email)
-  #     .update_all(user_id: self.id)
-  # end
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # devise_for :users
 
   root "events#index"
 
   resources :events
-    # resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :destroy]
   
-    # вложенный ресурс подписок
-    # resources :subscriptions, only: [:create, :destroy]
-  # end
+    resources :subscriptions, only: [:create, :destroy]
+  end
 
   resources :users, only: [:show, :edit, :update, :destroy]
 end
