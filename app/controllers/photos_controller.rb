1af92e0eeb0ffe996d5a99cc50125803d5cbb4aa
juwpan <juwpan@gmail.com>
2022-06-13 18:27:43 +0400

    database

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -31,7 +31,7 @@ class PhotosController < ApplicationController
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq - [photo.user.email].uniq
   
     all_emails.each do |mail|
-      EventMailer.photo(event, photo, mail).deliver_later
+      EventMailer.photo(event, photo, mail).deliver_now
     end
   end
 
