77da2bd65f2e519fc7652a45338487ac4e094ba0
juwpan <juwpan@gmail.com>
2022-06-14 15:58:24 +0400

    event parameter removed

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,17 +1,17 @@
 class EventMailer < ApplicationMailer
-  def photo(event, photo, email)
-    @event = event
+  def photo(photo, email)
+    @event = photo.event
     @name = photo.user.name
 
-    mail(to: email, subject: default_i18n_subject(event: event.title))
+    mail(to: email, subject: default_i18n_subject(event: photo.event.title))
   end
 
-  def subscription(event, subscription)
+  def subscription(subscription)
     @email = subscription.user_email
     @name = subscription.user_name
-    @event = event
+    @event = subscription.event
 
-    mail(to: @email, subject: default_i18n_subject(event: event.title))
+    mail(to: @email, subject: default_i18n_subject(event: subscription.event.title))
   end
 
   def comment(comment, email)
