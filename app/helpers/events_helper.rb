a805c52d7dfb185e998e6545c42386f2f0ab789c
juwpan <juwpan@gmail.com>
2022-06-15 22:02:47 +0400

    Event subscribe

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def subscriptions_user?(event, current_user)
+    event.subscriptions.each do |user|
+      if user_signed_in? && current_user.email == user.user_email && current_user.email
+        return true
+      end
+    end
+  end
 end
