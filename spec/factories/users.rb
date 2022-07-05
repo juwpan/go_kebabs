44650212da7dd7ec2cd80b6d437dab4299602207
juwpan <juwpan@gmail.com>
2022-07-06 01:18:34 +0400

    correct user policy factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "Ебаря_Гиёма_де_Ломарк_#{rand(999)}" }
+    name { "Гиёма_де_Ломарк_#{rand(999)}" }
     sequence(:email) { |n| "juwpan#{n}@gmail.com" }
     after(:build) { |u| u.password_confirmation = u.password = "1"}
   end
