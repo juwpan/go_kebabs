3d32414662bd65722a7572d7b6f9d4e7c83d990b
juwpan <juwpan@gmail.com>
2022-07-06 18:37:01 +0400

    database.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,13 +90,13 @@ Rails.application.configure do
 
   # config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.perform_deliveries = true
-  # # config.action_mailer.delivery_method = :mailjet
-  # config.action_mailer.delivery_method = :sendmail
-  # config.action_mailer.default_url_options = { host: 'copbygp.ru' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :mailjet
-  config.action_mailer.default_url_options = { host: 'https://bbq-juwpan.herokuapp.com' }
+  # config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: 'copbygp.ru' }
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.delivery_method = :mailjet
+  # config.action_mailer.default_url_options = { host: 'https://bbq-juwpan.herokuapp.com' }
 end
